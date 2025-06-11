8ec847ad3754d370862f4f7d8e953fbcf6f34a0b
fengsuimaster <3374252434@qq.com>
2025-06-11 16:25:07 +0800

    基本文档，开发板AD工程，新建的CUBEMX工程

---
+++ STM32G474CBT6_CAN_HAL/MDK-ARM/RTE/_STM32G474CBT6_CAN_HAL/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32G474CBT6_CAN_HAL' 
+ * Target:  'STM32G474CBT6_CAN_HAL' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
